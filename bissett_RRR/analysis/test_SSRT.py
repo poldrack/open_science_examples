fbe8f06faf5448ca3c1ab6f4bfcec6e5f9f0a8e3
poldrack <poldrack@gmail.com>
2020-06-26 16:17:01 -0700

    initial add

---
+++ bissett_RRR/analysis/test_SSRT.py
@@ -0,0 +1,36 @@
+"""
+test stop signal estimation procedure
+"""
+
+from get_SSRT import sim_stop_data, get_SSRT
+import numpy as np
+import pandas as pd
+
+
+def test_SSRT_estimation(threshold=0.98, nruns=100,
+                         meanSSRT=0.5, sdSSRT=0.4,
+                         ntrials=1000):
+    """
+    test ssrt estimation by simulating
+    data with varying SSRT and p_correct
+    """
+
+    results = []
+    SSRT = np.random.rand(nruns) * sdSSRT + meanSSRT
+    SSRT = SSRT + np.min(SSRT)
+
+    p_correct = 1 - np.random.rand(nruns) * .1
+
+    for i in range(nruns):
+        simdata = sim_stop_data(
+            SSRT=SSRT[i],
+            p_correct=p_correct[i],
+            ntrials=ntrials)
+
+        results.append(get_SSRT(simdata))
+
+    df = pd.DataFrame(results)
+    cc_SSRT = np.corrcoef(df.SSRT, SSRT)[0, 1]
+    assert cc_SSRT > threshold
+    cc_pcorr = np.corrcoef(df.pCorrectGoResp, p_correct)[0, 1]
+    assert cc_pcorr > threshold
