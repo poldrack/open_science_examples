a93989ebc55e42ef1b9d70aaa837f7913eff6ce8
poldrack <poldrack@gmail.com>
2020-08-05 14:09:37 -0700

    use functions from utils

--- IID/analysis/organize_data.py
+++ IID/analysis/organize_data.py
@@ -1,40 +1,22 @@
 """
-organize data for Direct Replication 1
+organize data for Direct Replication 2
 from original data files (obtain from OSF)
 into Psych-DS (ish) format
 
-study-1/
-    study-1_sub-<subnum>_data.tsv
+study-2/
+    study-2_sub-<subnum>_data.tsv
 """
 
 
-from pathlib import Path
-import pandas as pd
-
-basedir = Path('..')
-rawdir = basedir / 'raw_data'
-sourcedir = rawdir / 'source_data/wessel_replication_2_OSF'
-assert sourcedir.exists()
-
-outdir = rawdir / 'study-2'
-if not outdir.exists():
-    outdir.mkdir()
-
-logdir = Path('logs')
-if not logdir.exists():
-    logdir.mkdir()
+from utils import (get_command_line_arguments,
+                   get_directories,
+                   get_datafiles,
+                   load_and_resave_datafiles)
 
 
-# get data files
-
-datafiles = [i for i in sourcedir.glob('*.csv')]
-
-# get new filename/paths for each data file
-
-rename_dict = {}
-
-for d in datafiles:
-    data = pd.read_csv(d, sep='\t', index_col=0)
-    subcode = d.stem.split('_')[0]
-    outfile = outdir / ('study-2_sub-%s_data.tsv' % subcode)
-    data.to_csv(outfile, sep='\t')
+if __name__ == "__main__":
+    args = get_command_line_arguments()
+    args = get_directories(
+        args)
+    args = get_datafiles(args)
+    load_and_resave_datafiles(args)
