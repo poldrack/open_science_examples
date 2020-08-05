91d69818fe61f43d9415d8cffa7cbf0121b8eeca
poldrack <poldrack@gmail.com>
2020-08-05 16:51:27 -0700

    make studynum and studyname optional

--- IID/analysis/organize_data.py
+++ IID/analysis/organize_data.py
@@ -7,11 +7,25 @@ study-2/
     study-2_sub-<subnum>_data.tsv
 """
 
-
-from utils import (get_command_line_arguments,
-                   get_directories,
+import argparse
+from utils import (get_directories,
                    get_datafiles,
                    load_and_resave_datafiles)
+
+
+def get_command_line_arguments():
+    parser = argparse.ArgumentParser(
+        description='Reorganize replication data')
+    parser.add_argument('-b', '--basedir', required=True,
+                        help='base directory')
+    parser.add_argument('--studynum',
+                        default=1,
+                        type=int,
+                        help='study number')
+    parser.add_argument('--studyname',
+                        default='wessel_replication_1_OSF',
+                        help='study name')
+    return(parser.parse_args())
 
 
 if __name__ == "__main__":
